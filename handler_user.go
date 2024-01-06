45a0a502e4e8b3a867f80eb5fcc95f71bdca3094
Efe Kaya <sithban2@gmail.com>
2024-01-05 19:43:45 -0700

    Update handler_user.go

--- handler_user.go
+++ handler_user.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/rockswe/rssaggregator/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `name`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Printf("Error parsing JSON:", err))
+		return
+	}
+
+	apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID: uuid.New().String(),
+	})
+
+	respondwithJSON(w, 200, struct{}{})
+}
